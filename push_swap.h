e99baf0a3764ac16ebc8a7cb5f21169067ecb34c
axel lukongo <axellukongo@MacBook-Air-de-axel.local>
2022-04-01 00:05:38 +0200

    prototype

---
+++ push_swap.h
@@ -0,0 +1,11 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+typedef struct s_list
+{
+	
+} t_list;
+
+
+#endif // !PUSH_SWAP_H#define PUSH_SWAP_H
+
---
+++ sort.c
@@ -0,0 +1,17 @@
+#include "push_swap.h"
+
+void sort_sa()
+{
+}
+
+void sort_sb()
+{
+}
+
+void sort_ss()
+{
+}
+
+void sort_pa()
+{
+}
